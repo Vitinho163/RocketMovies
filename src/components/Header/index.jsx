75b0dd6b29bff184d204c12a854bb5ffb82e6819
João Victor <victor.jvsb18@gmail.com>
2023-12-01 18:15:43 -0300

    Implement open note

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,15 +5,31 @@ import { api } from '../../services/api';
 
 import { Link } from 'react-router-dom'
 import { Input } from '../Input'
-export function Header() {
+import { useEffect, useState } from 'react';
+export function Header({ onNotesUpdate }) {
     const { signOut, user } = useAuth();
+    const [search, setSearch] = useState("");
+    const [ notes, setNotes ] = useState([]);
 
     const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+
+    useEffect(() => {
+        async function fetchNotes() {
+            const response = await api.get(`/notes?title=${search}`);
+            setNotes(response.data);
+            onNotesUpdate(response.data);
+        }
+
+        fetchNotes();
+    }, [search]);
 
     return (
         <Container>
             <h1>Rocketmovies</h1>
-            <Input placeholder="Pesquisar pelo título" />
+            <Input 
+                placeholder="Pesquisar pelo título" 
+                onChange={(e) => setSearch(e.target.value)}
+            />
             <Profile to="/profile">
                 <div>
                     <strong>{user.name}</strong>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,11 +3,24 @@ import { Link } from 'react-router-dom';
 import { Header } from "../../components/Header";
 import { Movie } from '../../components/Movie';
 import { Container } from "./styles"
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function Home() {
+    const [notes, setNotes] = useState([]);
+    const navigate = useNavigate();
+
+    const handleNotesUpdate = (newNotes) => {
+        setNotes(newNotes);
+    };
+
+    function handleDetails(id) {
+        navigate(`/preview/${id}`);
+    }
+
     return (
         <Container>
-            <Header />
+            <Header onNotesUpdate={handleNotesUpdate} />
             <div className='mainTitle'>
                 <h1>Meus filmes</h1>
                 <Link to="/new">
@@ -16,35 +29,19 @@ export function Home() {
                 </Link>
             </div>
             <div className="movieTags">
-                <Link to='/preview/1'>
-                    <Movie data={{
-                        title: "Interestellar", tags: [
-                            { id: '1', name: 'Ficção Cientifica' },
-                            { id: '2', name: 'Drama' },
-                            { id: '3', name: 'Familia' }
-                        ]
-                    }} />
-                </Link>
+            {
+                notes.map(note => (
+                <Movie  
+                    key={String(note.id)}
+                    data={note}
+                    onClick={() => handleDetails(note.id)}
+                />
+            ))
+            }
 
-                <Link to='/preview/2'>
-                    <Movie data={{
-                        title: "Interestellar", tags: [
-                            { id: '1', name: 'Ficção Cientifica' },
-                            { id: '2', name: 'Drama' },
-                            { id: '3', name: 'Familia' }
-                        ]
-                    }} />
-                </Link>
+                
 
-                <Link to='/preview/3'>
-                    <Movie data={{
-                        title: "Interestellar", tags: [
-                            { id: '1', name: 'Ficção Cientifica' },
-                            { id: '2', name: 'Drama' },
-                            { id: '3', name: 'Familia' }
-                        ]
-                    }} />
-                </Link>
+                
 
             </div>
         </Container>
